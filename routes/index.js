25cba5bbd68b55863308c6cc356c560a9e37c27d
Imran <imran@loveairbear.com>
2017-02-14 12:36:31 -0500

    added new page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/push_data', function(req,res,next){
+    console.log(req.body)
+});
 
 module.exports = router;
