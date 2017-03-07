c968fa1170eac4cadc28784ae0b4fa4cf035fc67
Chad Blanchette <Chad Blanchette>
2017-03-07 14:03:13 -0500

    website update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path= require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,8 +12,9 @@ router.get('/datahook', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//router.get('/', function(req, res, next) {
- // res.render('index', { title: 'Express' });
-//});
+router.get('/deltaride', function(req, res, next) {
+ //res.render('index', { title: 'Express' });
+ res.sendFile(path.join(__dirname,'..','views/dashboard1.html'))
+});
 
 module.exports = router;
