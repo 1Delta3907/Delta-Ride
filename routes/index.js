d1fd238b2ec7adbcf31525161c013f4719453a5f
Michael Mikolainis <Michael Mikolainis>
2017-03-07 13:55:51 -0500

    datahook change for particle receive

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,15 @@ router.get('/', function(req, res, next) {
 });
 router.post('/push_data', function(req,res,next){
     console.log(req.body)
+    res.send("text")
 });
 
 router.get('/datahook', function(req, res, next) {
 	data = req.query
+	console.log(data)
   res.render('index', { title: 'Express' });
 });
 
-//router.get('/', function(req, res, next) {
- // res.render('index', { title: 'Express' });
-//});
 
 module.exports = router;
 
