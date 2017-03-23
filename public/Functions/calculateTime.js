136a4121a95d0ff0f8ef325e13e2cfce26563f60
Michael Mikolainis <michael.mikolainis@gmail.com>
2017-03-23 13:13:31 -0400

    Update calculateTime.js

--- public/Functions/calculateTime.js
+++ public/Functions/calculateTime.js
@@ -1,6 +1,11 @@
-function calculateTime (data){
+//calculate total time
+var csv = require("fast-csv");
+var totalTime
+r = csv.reader(open("file.csv","rb"))
 
-
-
+totalTime = 0
+for row in cr:  
+   totalTime += int(row[2])
+ 
 	return totalTime
 }
