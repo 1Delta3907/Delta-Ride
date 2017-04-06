7ac27735675bbb15390bc8eb734c5280546b2262
Michael Mikolainis <Michael Mikolainis>
2017-04-06 00:33:13 -0400

    data saving correctly

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,11 @@ var combinedData = [];
 var dataSchema = new mongoose.Schema({
   "coreId": String,
   "time": Array,
-  "date": Date
-});
+  "date": Date,
+  "_id" : {type: String, select: false}
+}, { collection: 'bike_datas'});
 
 var newDataSchema = new mongoose.Schema({
-	"trip_number": Number,
   	"time": Array,
   	"date": Date
 }, { collection: 'Bike_Data'});
@@ -38,77 +38,51 @@ router.get('/datahook', function(req, res, next) {
 	if(data[0]== 'End'){
 
 		console.log(data);
-		//numPackets = data[1];
+		numPackets = data[1];
 		
-		//exitNum = (parseInt(data[1]));
+		exitNum = (parseInt(data[1])) + 1;
 
 
-		bike_data_set.find({coreId : '390022001447343338333633'}, function(err, bikedata){
+		bike_data_set.find({}, 'time', function(err, bikedata){
 			if (err){
 					console.log(err);
 			}
 			console.log("bike data is " + bikedata);
-			for (var data in bikedata) {
-				if(!bikedata.hasOwnProperty(data)){
-					continue;
-				}
-				console.log("bike data.time is " + bikedata);
-			}
+			
 			
 			//console.log("bike data.time is " + bikedata.time);
-			//localvariable = bikedata.time;
+			localvariable = bikedata;
 			console.log("localvariable is " + localvariable);
-				for (j = 0; j < localvariable.length; j++){
-					combinedData.push(localvariable[j]);
+			//localvariable = localvariable.time.split(",");
+			var newArray = localvariable.map(function(element){
+				return element.time;
+			});
+			console.log("new localvariable is " + newArray);
+			
+				for (j = 0; j < newArray.length; j++){
+					//console.log(localvariable[j]);
+					combinedData.push(newArray[j]);
 				}
 			console.log("combined data is equal to " + combinedData);
 
 			console.log("saving data");
-					var recordTotal = new Tbike_data_set({trip_number : 1, time: combinedData, date: new Date().toISOString()})
+					var recordTotal = new Tbike_data_set({trip_number : 1, time: combinedData, date: new Date().toISOString(),})
 					recordTotal.save(function(err, data){
 						if (err){
 							console.log(err);
 						}
 					});
-		});
 
-
-		
-
-		/*for (i =1; i <= numPackets; i++){
-			console.log('iteration of forloop : ' + i)
-
-			bike_data_set.findOne({trip_number: i}, function (err, bikedata){
-				if (err){
-					console.log(err);
-				}
-				//console.log(bikedata); //for testing
-				tripVariable = bikedata.trip_number;
-				localvariable = bikedata.time;
-				console.log(localvariable);
-				for (j = 0; j < localvariable.length; j++){
-					combinedData.push(localvariable[j]);
-					//console.log(localvariable[j]);
-				}	
-			console.log("combined data is equal to " + combinedData);
-			console.log(i);
-			console.log(exitNum);
-			if (i == exitNum){
-
-				console.log("saving data");
-					var recordTotal = new Tbike_data_set({trip_number : tripVariable, time: combinedData, date: new Date().toISOString()})
-					recordTotal.save(function(err, data){
-						if (err){
-							console.log(err);
-						}
-					});
-			}
-			});	
-		}
-*/
-		
-		/*if (combinedData != []) {
-			mongoose.connection.db.dropCollection('bike datas', function(err, result) {
+			//if (combinedData == newArray) {
+				/*console.log("deleting data");
+				bikedata.remove({}, function (err){
+					if (err){
+						console.log(err);
+					} else{
+						console.log("data deleted");
+					}
+				});*/
+			/*mongoose.connection.db.dropCollection('bike datas', function(err, result) {
 				if (err){
 					console.log(err);
 				}
@@ -117,9 +91,56 @@ router.get('/datahook', function(req, res, next) {
 				if (err){
 					console.log(err);
 				}
-			});
-		}	
-	*/	//remove all documents from bike datas collection
+			});*/
+		//}	
+		});
+
+		
+		/*
+		for (i =1; i <= numPackets; i++){
+			console.log('iteration of forloop : ' + i)
+			
+
+			bike_data_set.findOne({coreId : '390022001447343338333633'}, function (err, bikedata){
+				if (err){
+					console.log(err);
+				}
+				console.log(bikedata); //for testing
+				bike_data_set.remove(bikedata, function(err){
+					if(!err){
+						console.log("data deleted");
+						localvariable = bikedata.time;
+						console.log(localvariable);
+						for (j = 0; j < localvariable.length; j++){
+							combinedData.push(localvariable[j]);
+							console.log(localvariable[j]);
+						}	
+					}
+				});
+				//tripVariable = bikedata.trip_number;
+				
+				
+			
+
+			console.log("combined data is equal to " + combinedData);
+			console.log(i);
+			console.log(exitNum);
+			if (i == exitNum){
+
+				console.log("saving data");
+					var recordTotal = new Tbike_data_set({time: combinedData, date: new Date().toISOString()})
+					recordTotal.save(function(err, data){
+						if (err){
+							console.log(err);
+						}
+					});
+			}
+			});	
+		}
+		
+		*/
+		
+		//remove all documents from bike datas collection
 
 		
 	} else {
@@ -131,7 +152,7 @@ router.get('/datahook', function(req, res, next) {
 	//console.log(bikedata);
 	//console.log(data_num);
 
-	var record = new bike_data_set({coreId: coreid, time:bikedata, date: new Date().toISOString()});
+	var record = new bike_data_set({coreId: coreid, time:bikedata, date: new Date().toISOString(), _id: Math.random()});
 	record.save(function(err, data){
 		if (err){
 			console.log(err);
